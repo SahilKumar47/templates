a05caa15c1eaf44b729f298236000972eff2a488
Sahil Kumar <49636096+SahilKumar47@users.noreply.github.com>
2020-10-03 22:18:15 +0530

    Update main.js

--- jhankaar/js/main.js
+++ jhankaar/js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
   "use strict";
 
-  // Preloader (if the #preloader div exists)
+  // Preloader
   $(window).on('load', function () {
     if ($('#preloader').length) {
       $('#preloader').delay(100).fadeOut('slow', function () {
