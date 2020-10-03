5154a6c7d4b69d82e9bc4024baffbb5dc52b143c
Sahil Kumar <49636096+SahilKumar47@users.noreply.github.com>
2020-10-03 22:19:04 +0530

    Update main.js

--- jhankaar/js/main.js
+++ jhankaar/js/main.js
@@ -17,7 +17,7 @@
     } else {
       $('.back-to-top').fadeOut('slow');
     }
-  });
+  }); 
   $('.back-to-top').click(function(){
     $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
     return false;
